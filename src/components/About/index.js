410babe964c793e09529225fdd4cec9fb6073b82
Banksandrew16 <88170202+Banksandrew16@users.noreply.github.com>
2022-01-13 14:12:28 -0500

    about

---
+++ src/components/About/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import personalPhoto from 
+
+function About() {
+    return (
+        <section className="my-5 main">
+            <h1 id="about">Who am I?</h1>
+
+            <div className="bio">
+                
+                <div>
+                    <p>My name is Andrew Banks. I have my business degree in sports managment. I have worked for the Cleveland Indians and San Diego Padres.</p>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default About;
---
+++ src/components/Footer/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer>
+            <ul>
+                <li>Phone: 317-616-8253</li>
+                <li>
+                    Email:
+                    <a
+                        href="mailto:banksandrew16@gmail.com"
+                        target="_blank" rel="noreferrer">
+                        Banksandrew16@gmail.com
+                    </a>
+                </li>
+                <li>
+                    GitHub:
+                    <a href="https://github.com/Banksandrew16" target="_blank" rel="noreferrer">
+                        github.com/Banksandrew16
+                    </a>
+                </li>
+                
+            </ul>
+
+        </footer>
+    )
+}
+
+export default Footer;
